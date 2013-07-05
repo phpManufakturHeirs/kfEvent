5e02f3790d8335ca77867f500360a857c8577263
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-07-05 08:44:20 +0200

    initial release 2.0.10

---
+++ bootstrap.include.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Contact
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://kit2.phpmanufaktur.de/FacebookGallery
+ * @copyright 2013 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+
+// scan the /Locale directory and add all available languages
+$app['utils']->addLanguageFiles(MANUFAKTUR_PATH.'/Event/Data/Locale');
+
