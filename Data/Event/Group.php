59a7fe20ae6464461937e6c36397cae0f79fbd52
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-07-20 10:16:29 +0200

    added different SQL queries for handling groups

--- Data/Event/Group.php
+++ Data/Event/Group.php
@@ -47,6 +47,7 @@ class Group
         `group_organizer_contact_tags` VARCHAR(512) NOT NULL DEFAULT '',
         `group_location_contact_tags` VARCHAR(512) NOT NULL DEFAULT '',
         `group_participant_contact_tags` VARCHAR(512) NOT NULL DEFAULT '',
+        `group_extra_fields` VARCHAR(512) NOT NULL DEFAULT '',
         `group_status` ENUM('ACTIVE', 'LOCKED', 'DELETED') NOT NULL DEFAULT 'ACTIVE',
         `group_timestamp` TIMESTAMP,
         PRIMARY KEY (`group_id`)
@@ -65,5 +66,134 @@ EOD;
         }
     }
 
+    public function getDefaultRecord()
+    {
+        return array(
+            'group_id' => -1,
+            'group_name' => '',
+            'group_description' => '',
+            'group_organizer_contact_tags' => '',
+            'group_location_contact_tags' => '',
+            'group_participant_contact_tags' => '',
+            'group_extra_fields' => '',
+            'group_status' => 'ACTIVE',
+            'group_timestamp' => '0000-00-00 00:00:00'
+        );
+    }
 
+    public function selectAll($status='DELETED', $status_operator='!=')
+    {
+        try {
+            $results = $this->app['db']->fetchAll("SELECT * FROM `".self::$table_name."` WHERE `group_status`{$status_operator}'{$status}' ORDER BY `group_name` ASC");
+            $groups = array();
+            if (is_array($results)) {
+                foreach ($results as $result) {
+                    $record = array();
+                    foreach ($result as $key => $value) {
+                        $record[$key] = is_string($value) ? $this->app['utils']->unsanitizeText($value) : $value;
+                    }
+                    $groups[] = $record;
+                }
+            }
+            return $groups;
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    /**
+     * Select a group record by the given group_id
+     * Return FALSE if the record does not exists
+     *
+     * @param integer $group_id
+     * @throws \Exception
+     * @return multitype:array|boolean
+     */
+    public function select($group_id)
+    {
+        try {
+            $SQL = "SELECT * FROM `".self::$table_name."` WHERE `group_id`='$group_id'";
+            $result = $this->app['db']->fetchAssoc($SQL);
+            if (is_array($result) && isset($result['group_id'])) {
+                $group = array();
+                foreach ($result as $key => $value) {
+                    $group[$key] = is_string($value) ? $this->app['utils']->unsanitizeText($value) : $value;
+                }
+                return $group;
+            }
+            else {
+                return false;
+            }
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    /**
+     * Check if the desired Group name already existst. Optionally exclude the
+     * given group id from the check
+     *
+     * @param string $group_name
+     * @param integer $exclude_group_id
+     * @throws \Exception
+     * @return boolean
+     */
+    public function existsGroupName($group_name, $exclude_group_id=null)
+    {
+        try {
+            $SQL = "SELECT `group_name` FROM `".self::$table_name."` WHERE `group_name`='$group_name'";
+            if (is_numeric($exclude_group_id)) {
+                $SQL .= " AND `group_id` != '$exclude_group_id'";
+            }
+            $result = $this->app['db']->fetchColumn($SQL);
+            return (strtoupper($result) == strtoupper($group_name)) ? true : false;
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    /**
+     * Insert a new group record
+     *
+     * @param array $data
+     * @param reference integer $group_id
+     * @throws \Exception
+     */
+    public function insert($data, &$group_id=null)
+    {
+        try {
+            $insert = array();
+            foreach ($data as $key => $value) {
+                if (($key == 'group_id') || ($key == 'group_timestamp')) continue;
+                $insert[$this->app['db']->quoteIdentifier($key)] = is_string($value) ? $this->app['utils']->unsanitizeText($value) : $value;
+            }
+            $this->app['db']->insert(self::$table_name, $insert);
+            $group_id = $this->app['db']->lastInsertId();
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    /**
+     * Update the group record for the given ID
+     *
+     * @param array $data
+     * @param integer $group_id
+     * @throws \Exception
+     */
+    public function update($data, $group_id)
+    {
+        try {
+            $update = array();
+            foreach ($data as $key => $value) {
+                if (($key == 'group_id') || ($key == 'group_timestamp') || ($key == 'group_name')) continue;
+                $update[$this->app['db']->quoteIdentifier($key)] = is_string($value) ? $this->app['utils']->sanitizeText($value) : $value;
+            }
+            if (!empty($update)) {
+                $this->app['db']->update(self::$table_name, $update, array('group_id' => $group_id));
+            }
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
 }
