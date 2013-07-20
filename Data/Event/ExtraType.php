cb7a1702674673cbe36d47131526d7ed651e93be
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-07-20 10:15:43 +0200

    removed group_id and group_type_status (no longer needed)

--- Data/Event/ExtraType.php
+++ Data/Event/ExtraType.php
@@ -42,14 +42,12 @@ class ExtraType
         $SQL = <<<EOD
     CREATE TABLE IF NOT EXISTS `$table` (
         `extra_type_id` INT(11) NOT NULL AUTO_INCREMENT,
-        `group_id` INT(11) NOT NULL DEFAULT '-1',
         `extra_type_type` ENUM('TEXT','HTML','VARCHAR','INT','FLOAT','DATE','DATETIME','TIME') NOT NULL DEFAULT 'VARCHAR',
         `extra_type_name` VARCHAR(64) NOT NULL DEFAULT '',
         `extra_type_description` TEXT NOT NULL,
-        `extra_type_status` ENUM('ACTIVE', 'LOCKED', 'DELETED') NOT NULL DEFAULT 'ACTIVE',
         `extra_type_timestamp` TIMESTAMP,
         PRIMARY KEY (`extra_type_id`),
-        INDEX (`group_id`)
+        UNIQUE (`extra_type_name`)
         )
     COMMENT='The table for definition of types for extra fields'
     ENGINE=InnoDB
@@ -65,5 +63,193 @@ EOD;
         }
     }
 
+    public function getDefaultRecord()
+    {
+        return array(
+            'extra_type_id' => -1,
+            'extra_type_type' => '',
+            'extra_type_name' => '',
+            'extra_type_description' => '',
+            'extra_type_timestamp' => '0000-00-00 00:00:00'
+        );
+    }
+
+    public function getTypeArrayForTwig()
+    {
+        return array(
+            'TEXT' => 'Text - plain',
+            'HTML' => 'Text - HTML',
+            'VARCHAR' => 'Text - 256 characters',
+            'INT' => 'Integer',
+            'FLOAT' => 'Float',
+            'DATE' => 'Date',
+            'DATETIME' => 'Date and Time'
+        );
+    }
+
+    /**
+     * Check if the desired Extra Type already existst. Optionally exclude the
+     * given ID from the check
+     *
+     * @param string $type_name
+     * @param integer $exclude_type_id
+     * @throws \Exception
+     * @return boolean
+     */
+    public function existsTypeName($type_name, $exclude_type_id=null)
+    {
+        try {
+            $SQL = "SELECT `extra_type_name` FROM `".self::$table_name."` WHERE `extra_type_name`='$type_name'";
+            if (is_numeric($exclude_type_id)) {
+                $SQL .= " AND `extra_type_id` != '$exclude_type_id'";
+            }
+            $result = $this->app['db']->fetchColumn($SQL);
+            return (strtoupper($result) == strtoupper($type_name)) ? true : false;
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    /**
+     * Insert a new extra type record
+     *
+     * @param array $data
+     * @param reference integer $type_id
+     * @throws \Exception
+     */
+    public function insert($data, &$type_id=null)
+    {
+        try {
+            $insert = array();
+            foreach ($data as $key => $value) {
+                if (($key == 'extra_type_id') || ($key == 'extra_type_timestamp')) continue;
+                $insert[$this->app['db']->quoteIdentifier($key)] = is_string($value) ? $this->app['utils']->unsanitizeText($value) : $value;
+            }
+            $this->app['db']->insert(self::$table_name, $insert);
+            $type_id = $this->app['db']->lastInsertId();
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    /**
+     * Update the extra type record for the given ID
+     *
+     * @param array $data
+     * @param integer $type_id
+     * @throws \Exception
+     */
+    public function update($data, $type_id)
+    {
+        try {
+            $update = array();
+            foreach ($data as $key => $value) {
+                if (($key == 'extra_type_id') || ($key == 'extra_type_timestamp') || ($key == 'extra_type_name')) continue;
+                $update[$this->app['db']->quoteIdentifier($key)] = is_string($value) ? $this->app['utils']->sanitizeText($value) : $value;
+            }
+            if (!empty($update)) {
+                $this->app['db']->update(self::$table_name, $update, array('extra_type_id' => $type_id));
+            }
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    /**
+     * Return a array with all tags, prepared for usage with TWIG
+     *
+     * @throws \Exception
+     * @return array
+     */
+    public function getArrayForTwig()
+    {
+        try {
+            $SQL = "SELECT * FROM `".self::$table_name."` ORDER BY `extra_type_name` ASC";
+            $results = $this->app['db']->fetchAll($SQL);
+            $types = array();
+            foreach ($results as $type) {
+                $types[$type['extra_type_name']] = ucfirst(str_replace('_', ' ', strtolower($type['extra_type_name'])));
+            }
+            return $types;
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    public function selectAll()
+    {
+        try {
+            $results = $this->app['db']->fetchAll("SELECT * FROM `".self::$table_name."` ORDER BY `extra_type_name` ASC");
+            $types = array();
+            if (is_array($results)) {
+                foreach ($results as $result) {
+                    $record = array();
+                    foreach ($result as $key => $value) {
+                        $record[$key] = is_string($value) ? $this->app['utils']->unsanitizeText($value) : $value;
+                    }
+                    $types[] = $record;
+                }
+            }
+            return $types;
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    /**
+     * Select a extra type record by the given extra_type_id
+     * Return FALSE if the record does not exists
+     *
+     * @param integer $extra_type_id
+     * @throws \Exception
+     * @return multitype:array|boolean
+     */
+    public function select($extra_type_id)
+    {
+        try {
+            $SQL = "SELECT * FROM `".self::$table_name."` WHERE `extra_type_id`='$extra_type_id'";
+            $result = $this->app['db']->fetchAssoc($SQL);
+            if (is_array($result) && isset($result['extra_type_id'])) {
+                $type = array();
+                foreach ($result as $key => $value) {
+                    $type[$key] = is_string($value) ? $this->app['utils']->unsanitizeText($value) : $value;
+                }
+                return $type;
+            }
+            else {
+                return false;
+            }
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
+
+    /**
+     * Select a extra type record by the given extra_type_name
+     * Return FALSE if the record does not exists
+     *
+     * @param integer $extra_type_name
+     * @throws \Exception
+     * @return multitype:array|boolean
+     */
+    public function selectName($extra_type_name)
+    {
+        try {
+            $SQL = "SELECT * FROM `".self::$table_name."` WHERE `extra_type_name`='$extra_type_name'";
+            $result = $this->app['db']->fetchAssoc($SQL);
+            if (is_array($result) && isset($result['extra_type_name'])) {
+                $type = array();
+                foreach ($result as $key => $value) {
+                    $type[$key] = is_string($value) ? $this->app['utils']->unsanitizeText($value) : $value;
+                }
+                return $type;
+            }
+            else {
+                return false;
+            }
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw new \Exception($e);
+        }
+    }
 
 }
