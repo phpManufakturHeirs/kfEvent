6d6eb7aaffa93bc02aa0f9d619c7dcde3e665716
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-08-13 17:33:18 +0200

    removed obsolete use statements

--- bootstrap.include.php
+++ bootstrap.include.php
@@ -10,11 +10,6 @@
  */
 
 use phpManufaktur\Basic\Control\CMS\EmbeddedAdministration;
-use phpManufaktur\Event\Control\Backend\GroupList as EventGroupList;
-use phpManufaktur\Event\Control\Backend\GroupEdit as EventGroupEdit;
-use phpManufaktur\Event\Control\Backend\ExtraFieldList as EventExtraFieldList;
-use phpManufaktur\Event\Control\Backend\ExtraFieldEdit as EventExtraFieldEdit;
-use phpManufaktur\Event\Control\Backend\EventEdit;
 
 // scan the /Locale directory and add all available languages
 $app['utils']->addLanguageFiles(MANUFAKTUR_PATH.'/Event/Data/Locale');
@@ -35,91 +30,91 @@ $app->get('/event/cms/{cms_information}', function ($cms_information) use ($app)
 });
 
 // About dialog
-$app->get('/admin/event/about', 
+$app->get('/admin/event/about',
     'phpManufaktur\Event\Control\Backend\About::exec');
 
 // Contact List
-$app->match('/admin/event/contact/list', 
+$app->match('/admin/event/contact/list',
     'phpManufaktur\Event\Control\Backend\ContactList::exec');
-$app->match('/admin/event/contact/list/page/{page}', 
+$app->match('/admin/event/contact/list/page/{page}',
     'phpManufaktur\Event\Control\Backend\ContactList::exec');
 
 // Contact create and edit
-$app->match('/admin/event/contact/select', 
+$app->match('/admin/event/contact/select',
     'phpManufaktur\Event\Control\Backend\ContactSelect::exec');
-$app->match('/admin/event/contact/edit/id/{contact_id}', 
+$app->match('/admin/event/contact/edit/id/{contact_id}',
     'phpManufaktur\Event\Control\Backend\ContactSelect::exec');
 
 // Create and Edit Person contacts
-$app->match('/admin/event/contact/person/edit', 
+$app->match('/admin/event/contact/person/edit',
     'phpManufaktur\Event\Control\Backend\ContactPerson::exec');
-$app->match('/admin/event/contact/person/edit/id/{contact_id}', 
+$app->match('/admin/event/contact/person/edit/id/{contact_id}',
     'phpManufaktur\Event\Control\Backend\ContactPerson::exec');
 
 // Create and Edit Company contacts
-$app->match('/admin/event/contact/company/edit', 
+$app->match('/admin/event/contact/company/edit',
     'phpManufaktur\Event\Control\Backend\ContactCompany::exec');
-$app->match('/admin/event/contact/company/edit/id/{contact_id}', 
+$app->match('/admin/event/contact/company/edit/id/{contact_id}',
     'phpManufaktur\Event\Control\Backend\ContactCompany::exec');
 
 // Category List
-$app->match('/admin/event/contact/category/list', 
+$app->match('/admin/event/contact/category/list',
     'phpManufaktur\Event\Control\Backend\Contact\CategoryList::exec');
 
 // Category Edit
-$app->match('/admin/event/contact/category/edit', 
+$app->match('/admin/event/contact/category/edit',
     'phpManufaktur\Event\Control\Backend\Contact\CategoryEdit::exec');
-$app->match('/admin/event/contact/category/edit/id/{category_id}', 
+$app->match('/admin/event/contact/category/edit/id/{category_id}',
     'phpManufaktur\Event\Control\Backend\Contact\CategoryEdit::exec');
 
 // Title List
-$app->match('/admin/event/contact/title/list', 
+$app->match('/admin/event/contact/title/list',
     'phpManufaktur\Event\Control\Backend\Contact\TitleList::exec');
 
 // Title Edit
-$app->match('/admin/event/contact/title/edit', 
+$app->match('/admin/event/contact/title/edit',
     'phpManufaktur\Event\Control\Backend\Contact\TitleEdit::exec');
-$app->match('/admin/event/contact/title/edit/id/{title_id}', 
+$app->match('/admin/event/contact/title/edit/id/{title_id}',
     'phpManufaktur\Event\Control\Backend\Contact\TitleEdit::exec');
 
 // Tag List
-$app->match('/admin/event/contact/tag/list', 
+$app->match('/admin/event/contact/tag/list',
     'phpManufaktur\Event\Control\Backend\Contact\TagList::exec');
 
 // Tag Edit
-$app->match('/admin/event/contact/tag/edit', 
+$app->match('/admin/event/contact/tag/edit',
     'phpManufaktur\Event\Control\Backend\Contact\TagEdit::exec');
-$app->match('/admin/event/contact/tag/edit/id/{tag_id}', 
+$app->match('/admin/event/contact/tag/edit/id/{tag_id}',
     'phpManufaktur\Event\Control\Backend\Contact\TagEdit::exec');
 
 // Event Group List
-$app->match('/admin/event/group/list', 
+$app->match('/admin/event/group/list',
     'phpManufaktur\Event\Control\Backend\GroupList::exec');
 
 // Event Group Edit
-$app->match('/admin/event/group/edit', 
+$app->match('/admin/event/group/edit',
     'phpManufaktur\Event\Control\Backend\GroupEdit::exec');
-$app->match('/admin/event/group/edit/id/{group_id}', 
+$app->match('/admin/event/group/edit/id/{group_id}',
     'phpManufaktur\Event\Control\Backend\GroupEdit::exec');
 
 // Extra Field List
-$app->match('/admin/event/extra/field/list', 
+$app->match('/admin/event/extra/field/list',
     'phpManufaktur\Event\Control\Backend\ExtraFieldList::exec');
 
 // Extra Field Edit
-$app->match('/admin/event/extra/field/edit', 
+$app->match('/admin/event/extra/field/edit',
     'phpManufaktur\Event\Control\Backend\ExtraFieldEdit::exec');
-$app->match('/admin/event/extra/field/edit/id/{type_id}', 
+$app->match('/admin/event/extra/field/edit/id/{type_id}',
     'phpManufaktur\Event\Control\Backend\ExtraFieldEdit::exec');
 
 // Create or Edit Event
-$app->match('/admin/event/edit', 
+$app->match('/admin/event/edit',
     'phpManufaktur\Event\Control\Backend\EventEdit::exec');
 $app->match('/admin/event/edit/id/{event_id}',
     'phpManufaktur\Event\Control\Backend\EventEdit::exec');
 
 // Show the Event List
-$app->match('/admin/event/list', 
+$app->match('/admin/event/list',
     'phpManufaktur\Event\Control\Backend\EventList::exec');
 
 
