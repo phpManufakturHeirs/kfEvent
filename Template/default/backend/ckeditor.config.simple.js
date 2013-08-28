c8e57197d11b5b604885ee0d47ced2a525568bc3
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-08-28 15:51:37 +0200

    fixed handling of html entities

--- Template/default/backend/ckeditor.config.simple.js
+++ Template/default/backend/ckeditor.config.simple.js
@@ -4,29 +4,35 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For the complete reference:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+    // Define changes to default configuration here.
+    // For the complete reference:
+    // http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbar = [
-	  	{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Paste', 'PasteText', 'PasteFromWord' ] },
-	  	{ name: 'editing', groups: [ 'find', 'selection' ] },
-	  	{ name: 'links', items: [ 'Link', 'Unlink' ] },
-	  	{ name: 'insert', items: [ 'Image', 'SpecialChar' ] },
-	  	{ name: 'others', items: [ '-' ] },
-	  	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', '-', 'RemoveFormat' ] },
+    // The toolbar groups arrangement, optimized for two toolbar rows.
+    config.toolbar = [
+          { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Paste', 'PasteText', 'PasteFromWord' ] },
+          { name: 'editing', groups: [ 'find', 'selection' ] },
+          { name: 'links', items: [ 'Link', 'Unlink' ] },
+          { name: 'insert', items: [ 'Image', 'SpecialChar' ] },
+          { name: 'others', items: [ '-' ] },
+          { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', '-', 'RemoveFormat' ] },
       { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source' ] }
-	  ];
+      ];
 
 
-	// Remove some buttons, provided by the standard plugins, which we don't
-	// need to have in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+    // Remove some buttons, provided by the standard plugins, which we don't
+    // need to have in the Standard(s) toolbar.
+    config.removeButtons = 'Underline,Subscript,Superscript';
 
-	// Se the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
+    // Se the most common block elements.
+    config.format_tags = 'p;h1;h2;h3;pre';
 
-	// Make dialogs simpler.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
+    // Make dialogs simpler.
+    config.removeDialogTabs = 'image:advanced;link:advanced';
+
+    // utf8 need no entities!
+    config.entities = false;
+    config.basicEntities = false;
+    config.entities_greek = false;
+    config.entities_latin = false;
 };
