11517bff1467fa2e79a2b2cddf6aaf2fa79fb952
hertsch <ralf.hertsch@phpmanufaktur.de>
2014-09-13 05:06:27 +0200

    updated `@link` references

--- Data/Locale/en.php
+++ Data/Locale/en.php
@@ -4,7 +4,7 @@
  * kitFramework::Event
  *
  * @author Team phpManufaktur <team@phpmanufaktur.de>
- * @link https://kit2.phpmanufaktur.de
+ * @link https://kit2.phpmanufaktur.de/Event
  * @copyright 2014 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  *
